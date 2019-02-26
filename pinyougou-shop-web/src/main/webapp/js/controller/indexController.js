f58d26f73195202145863af9d2334678877979de
zhongying <1142436215@qq.com>
2019-02-26 13:18:18 +0800

    品优购

---
+++ pinyougou-shop-web/src/main/webapp/js/controller/indexController.js
@@ -0,0 +1,11 @@
+ //品牌控制层 
+app.controller('indexController' ,function($scope,$controller,loginService){	
+	//读取当前登录人
+	$scope.showLoginName=function(){
+		loginService.loginName().success(
+			function(response) {
+				$scope.loginName=response.loginName;
+			}
+		);
+	}
+});	
